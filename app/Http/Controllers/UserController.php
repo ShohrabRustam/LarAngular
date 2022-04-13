51b85afe54c1ee70d9531879f9da93a8d89c0f9c
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-14 01:56:32 +0530

    form with api working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -8,16 +9,25 @@ use App\Models\User;
 class UserController extends Controller
 {
     //
-    public function register(Request $request){
-        $user = User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=> bcrypt($request->password)
+    public function register(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
 
-        ]);
-        $response['status']=1;
-        $response['message']="User Register Successfully";
-        $response['code']=200;
+        if ($user) {
+            $response['status'] = 0;
+            $response['message'] = "Email Already Exists";
+            $response['code'] = 409;
+        } else {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+
+            ]);
+            $response['status'] = 1;
+            $response['message'] = "User Register Successfully";
+            $response['code'] = 200;
+        }
         return response()->json($response);
     }
 }
