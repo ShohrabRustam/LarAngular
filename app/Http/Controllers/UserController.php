41f740435f3ec13c85b2db4f104c9d90ef1a24f5
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-14 11:24:47 +0530

    getting the error for the declare of claimn

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Dflydev\DotAccessData\Data;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
 
 class UserController extends Controller
 {
@@ -22,12 +26,44 @@ class UserController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => bcrypt($request->password)
-
             ]);
             $response['status'] = 1;
             $response['message'] = "User Register Successfully";
             $response['code'] = 200;
         }
+        return response()->json($response);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials=$request->only('email','password');
+        try{
+            if(!JWTAuth::attempt($credentials)){
+            $response ['status']=0;
+            $response ['code']=401;
+            $response ['message']="Email or Password Incorrect !!";
+            }
+
+        }catch(JWTException $e){
+            $response ['data']=null;
+            $response ['code']=500;
+            $response ['message']="could not create tokken !!";
+        }
+        $user = auth()->user();
+
+        $data['tokken']=auth()->claims(['user_id'=>$user->id, 'email'=>$user->email])->attempt($credentials);
+        $user = auth()->user();
+        // $data['token']= auth()->claims([
+        // 'user_id' => $user->id,
+        // 'email' => $user->email
+        // ])->attempt($credentials);
+
+
+        // return $response;
+        // $response['data']= $data;
+        $response ['status']=1;
+        $response ['code']=200;
+        $response ['message']="Login Successfully !!";
         return response()->json($response);
     }
 }
